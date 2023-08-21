a7a164be783e1f53be1cc2b7671d208204d40161
ygbadamosi662 <gbadamosiy662@gmail.com>
2023-08-21 13:36:56 +0100

    okay

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -35,7 +35,6 @@ class Server:
         """
         if self.__indexed_dataset is None:
             dataset = self.dataset()
-            truncated_dataset = dataset[:1000]
             self.__indexed_dataset = {
                 i: dataset[i] for i in range(len(dataset))
             }
