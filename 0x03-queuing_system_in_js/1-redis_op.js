81278f5049f107ea9d7bb9037edbcb4025bc2ef0
ygbadamosi662 <gbadamosiy662@gmail.com>
2023-09-25 12:51:07 +0100

    okay

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,29 @@
+import { createClient, print } from "redis";
+
+const client = createClient();
+
+client
+  .on("connect", () => {
+    console.log("Redis client connected to the server");
+  })
+  .on("error", (error) => {
+    console.log(`Redis client not connected to the server: ${error}`);
+  });
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, print);
+}
+
+function displaySchoolValue(schoolName) {
+  client.get(schoolName, (err, res) => {
+    if (err) {
+      console.log(err);
+      throw err;
+    }
+    console.log(res);
+  });
+}
+
+displaySchoolValue("Holberton");
+setNewSchool("HolbertonSanFrancisco", "100");
+displaySchoolValue("HolbertonSanFrancisco");
